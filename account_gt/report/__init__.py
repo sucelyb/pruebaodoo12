626669f76253d8f89b48a6d4007faba964c7d9ac
Sucely Batz <sucelybch19@gmail.com>
2021-06-14 08:44:42 -0600

    no

--- account_gt/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from . import libro_compras_report
-from . import libro_ventas_report
-from . import libro_bancos_report
-from . import libro_conciliacion_bancaria_report
